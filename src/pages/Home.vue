9853f2cfc5bed577ff9c862ea9992c71d190a802
Paul Doyle <71635533+N00193346@users.noreply.github.com>
2021-11-20 15:22:34 +0000

    first commit

---
+++ src/pages/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <b-col>
+    <h2>Welcome to the Cloud Festivals Website</h2>
+
+    <div v-if="!loggedIn">
+    <h4>Email</h4>
+    <input type="email" v-model="form.email" />
+    <br>
+     <h4>Password</h4>
+     <input type="password" v-model="form.password" />
+     <br>
+     <button @click="login(form)">Submit</button>
+    </div>
+    <p v-else>
+      You are logged in 
+    </p>
+  </b-col>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: "Home",
+  components: {
+
+  },
+  data() {
+    return {
+      form : {
+        email: "admin@festivals.ie",
+        password: "secret",
+      },
+  
+    }
+  },
+  computed: {
+    ...mapState(['loggedIn'])
+  },
+  methods:{
+    ...mapActions(['login']),
+  }
+};
+</script>
